43a137b35b6b79a75348f519ba4f223be58f5e39
MarkusJorroch <markusactionjackson@gmail.com>
2019-12-12 14:02:22 +0100

    Holiday mit fremd service

---
+++ server/inc/db.php
@@ -0,0 +1,7 @@
+<?php
+echo date("H:i:s");
+
+$db = new mysqli('localhost', 'root', 'root', 'todolist');
+
+
+?>
---
+++ server/myFirstPhp.php
@@ -0,0 +1,12 @@
+<?php
+require 'inc/db.php';
+?>
+
+<html>
+ <head>
+  <title>PHP-Test</title>
+ </head>
+ <body>
+ <?php echo '<p>Hallo Bobo1</p>'; ?>
+ </body>
+</html>
